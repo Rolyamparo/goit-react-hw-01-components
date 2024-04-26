6e751453e09fddbacb19290790cbbeffe3ddba05
Rolyamparo <143687528+Rolyamparo@users.noreply.github.com>
2024-04-26 20:10:12 +0800

    initial commit

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>invoice</td>
+          <td>125</td>
+          <td>USD</td>
+
+        </tr>
+            
+          
+      </tbody>
+    </table>
+  );
+ 
+};
+
+
